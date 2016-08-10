6726d1ffbe58920eedfe9b01e78d6ae99e4885ce
DylanRothko <dylan@dylangaines.com>
2016-08-09 17:00:20 -0700

    Trying to build some more non-opinionated code for sensible defaults

---
+++ page.home.php
@@ -0,0 +1,8 @@
+<?php 
+  /* 
+  // Template Name: Home Page
+  */ 
+  get_header();
+  get_template_part( 'includes/content/page', 'content' );
+  get_footer();
+?>
---
+++ page.single.php
@@ -0,0 +1,36 @@
+<?php 
+  /* 
+  // Template Name: Single Page Parent
+  */
+  
+  // 
+  // Lets globalize our post variable so
+  // that we can find the menu with the
+  // same name
+  //
+  global $post;
+  $post_slug = $post->post_name;  
+  $menu_name = $post_slug;
+  $locations = get_nav_menu_locations();
+  $menu = wp_get_nav_menu_object( $menu_name );
+  $menuitems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) );    
+  get_header();
+  //
+  // START SINGLE PAGE MENU ITEM LOOP
+  foreach( $menuitems as $item ):
+    switch ($item->object) {
+      //
+      // If this menu item is for a category 
+      // we'll put that logic here
+      case 'category':
+        break;
+      //
+      // Default logic
+      default:
+        break;
+    }
+  //
+  // END SINGLE PAGE MENU ITEM LOOP
+  endfor;
+  get_footer();
+?>
